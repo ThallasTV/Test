c51b2e9513a0566e2094d5ccafda674e07c93af4
Joshua Eatwell <josh-eatwell@hotmail.com>
2024-01-27 18:09:03 +0000

    An introduction to "Very Easy" challenges supplied by https://edabit.com

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿public class Program
+{
+
+    public int Sum(int a, int b)
+    {
+        return a+b;
+    }
+
+    public int Remainder(int x, int y)
+    {
+        return x % y;
+    }
+
+    public int TriArea(int b, int h)
+    {
+        return b*h/2;
+    }
+
+    public int Convert(int minutes)
+    {
+        return minutes * 60;
+    }
+
+    public bool LessThan100(int a, int b)
+    {
+        if(a + b < 100)
+            return true;
+        else
+            return false;
+    }
+    
+    public int Addition(int num)
+    {
+        return ++num;
+    }
+
+    public bool LessThanOrEqualToZero(double a) => a <= 0 ? true : false;
+
+    public string GiveMeSomething(string a)
+    {
+        return $"something {a}";
+    }
+    
+    // public int GetAbsSum(int[] arr)
+    // {
+        
+    // }
+    private static void Main(string[] args)
+    {
+        var chall = new Program();
+        Console.WriteLine("Challenge #1: " + chall.Sum(3,2));
+        Console.WriteLine("Challenge #2: " + chall.Remainder(1,3));
+        Console.WriteLine("Challenge #3: " + chall.TriArea(7,4));
+        Console.WriteLine("Challenge #4: " + chall.Convert(4));
+        Console.WriteLine("Challenge #5: " + chall.LessThan100(50,21));
+        Console.WriteLine("Challenge #6: " + chall.Addition(1));
+        Console.WriteLine("Challenge #7: " + chall.LessThanOrEqualToZero(0));
+        
+    }
+}
